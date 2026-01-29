267b36e2ea3dd276ba256c13e8f7173728ed68b8
gingerlolipop <jillblack@qq.com>
2026-01-29 10:14:05 -0800

    paste old script
    Added data processing steps for vegetation type and DEM retrieval.

--- script/1. data.R
+++ script/1. data.R
@@ -1,1 +1,49 @@
+library(CEMT)
+library(raster)
+library(rgdal)
 
+setwd("E:/Jing/ecoChina/play/China")
+
+## 1. convert veg type data to table---------
+r <- raster('raster/veg_3');r 
+#r2 <- raster('E:/Jing/ecoChina/play/China/raster/veg_chn_poly2ras/veg_chn_poly2ras.tif');r2 #new raster I downloaded: 1:1million
+crs(r) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" 
+
+
+writeRaster(r,'raster/ecosys_ori.tif',format="GTiff")
+
+xyv <- xyvFromRaster(r, attr=T,keepNA=F);hd(xyv)
+#Q:x = longitude, y = latitude, v = ?some row has v value == ecosys name, but others are empty
+
+id <-  xyvFromRaster(r, attr=F,keepNA=F);hd(id)
+print(sort(unique(id$VEGETATI_3))) #levels = 1:56
+
+xyv1 <- cbind(id, xyv[,3]);hd(xyv1)
+names(xyv1)[3:4] <- c('zoneID','zone');hd(xyv1)
+xyv1$zoneID <- as.factor(xyv1$zoneID);str(xyv1)
+xyv1 <- xyv1[complete.cases(xyv1[, 1:3]), ];hd(xyv1) #nothing deleted
+#revision: not remoging "" zone #xyv2 <- droplevels(xyv1[xyv1$zone!="",]);hd(xyv2) #remove "" zone
+
+#zone and zoneID ---
+lvl <- data.frame(levels(r));lvl
+# revision: no removing lvl2 <- droplevels(lvl[lvl$VEGETATI_3!="",]);hd(lvl2) #remove "" zone
+fWrite(lvl,'1. zoneID_zone_count.csv')
+
+rm(lvl,xyv,id,r2);gc()
+
+
+
+# get DEM----------------
+dem <- getDEM('chn',id[,c('x','y')],z=90) #this function does not work on my laptop, need to retrieve from 3333b
+
+id$id2 <- NA;hd(id)
+colnames(id) <- c("lon", "lat", "Vegetati_3", "id2");hd(id)
+id <- id[, c("Vegetati_3", "id2", "lat", "lon")];hd(id)
+
+dem <- cbind(id,dem);hd(dem)
+
+#output ClimateAP input file----
+crd <- data.frame(ID=row(dt)[,1],dt[,c(3,2,1)],dem);hd(crd)
+fWrite(dem,'1. coord.csv')
+
+fRead('1. coord.csv')
