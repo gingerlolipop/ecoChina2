e5f5e201748a0879d8c73f2225b1c3d8bb679d81
gingerlolipop <jillblack@qq.com>
2026-05-05 08:06:53 -0700

    dual suit function from before

---
+++ script/Dual Suit Caculation (Indicator Function).R
@@ -0,0 +1,21 @@
+# Define the dual suitability function
+# Formula: Suitability_dual  =I(Suitability_soil>threshold)* Suitability_climate+ I(Suitability_soil<threshold)  0
+# The indicator function I() can be represented using the as.integer() function
+# in R which will convert TRUE to 1 and FALSE to 0.
+
+# Define the dual suitability function
+calculate_dual_suitability <- function(climate, soil) {
+  # Apply the condition for suitability soil
+  ifelse(soil > threshold, climate, 0)
+}
+
+
+# Exaple threshold == 0.2
+threshold <- 0.2 
+
+# Apply the function
+dual_suitability <- overlay(pclim75, psoil75_masked, fun = calculate_dual_suitability)
+
+# Plot and save the result
+plot(dual_suitability)
+writeRaster(dual_suitability, "dual_suitability.tif", overwrite = TRUE)
